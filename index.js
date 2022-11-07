2e8031ee4ac8d19f7bbdca207af8b0e58623ddb1
Uttom <uttom.teacher.gps2@gmail.com>
2022-11-08 00:06:53 +0600

    Mongodb Atlas Database Connected Sucessfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -11,7 +12,26 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@assignment.zr6th40.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('Assignment').collection('services');
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await serviceCollection.insertOne(order);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Youtuber server is running')
